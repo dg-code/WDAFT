2b9588ca5bf88ec8fa18a41dd1f3c4749a0b0e00
dgjorgievski <dgjorgievski@live.com>
2016-08-22 22:55:45 +0200

    Updated the DriverType as TODO

--- src/test/java/com/dgcode/wdaft/config/DriverType.java
+++ src/test/java/com/dgcode/wdaft/config/DriverType.java
@@ -10,10 +10,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public enum DriverType implements DriverSetup{
     FIREFOX {
         public WebDriver getWebDriververObject(DesiredCapabilities desiredCapabilities) {
+            // TODO: 22.08.2016  
             return null;
         }
 
         public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
+            // TODO: 22.08.2016
             return null;
         }
     }
