cb7c0235e13502ac7b3424ac1774cbb9230b5563
dgjorgievski <dgjorgievski@live.com>
2016-08-25 23:33:42 +0200

    Update FIREFOX DriverType to get the desired capabilities and proxy settings

--- src/test/java/com/dgcode/wdaft/config/DriverType.java
+++ src/test/java/com/dgcode/wdaft/config/DriverType.java
@@ -2,21 +2,31 @@ package com.dgcode.wdaft.config;
 
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.openqa.selenium.remote.CapabilityType.PROXY;
 
 /**
  * Created by dgjorgievski on 22.08.2016.
  */
 public enum DriverType implements DriverSetup{
     FIREFOX {
-        public WebDriver getWebDriververObject(DesiredCapabilities desiredCapabilities) {
-            // TODO: 22.08.2016  
-            return null;
+        public WebDriver getWebDriverObject(DesiredCapabilities desiredCapabilities) {
+            return new FirefoxDriver(desiredCapabilities);
         }
 
         public DesiredCapabilities getDesiredCapabilities(Proxy proxySettings) {
-            // TODO: 22.08.2016
-            return null;
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            capabilities.setCapability("marionette", true);
+            return addProxySettings(capabilities, proxySettings);
         }
+    };
+
+    private static DesiredCapabilities addProxySettings(DesiredCapabilities capabilities, Proxy proxySettings) {
+        if (null != proxySettings) {
+            capabilities.setCapability(PROXY, proxySettings);
+        }
+        return capabilities;
     }
 }
