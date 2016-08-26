9ac3e94f47d08c41dea147c0a00b1c963929413a
dgjorgievski <dgjorgievski@live.com>
2016-08-26 23:07:55 +0200

    Delete the initial project files Update and restructure the code

--- src/test/java/com/dgcode/wdaft/TestBase.java
+++
@@ -1,7 +0,0 @@
-package com.dgcode.wdaft;
-
-/**
- * Created by danielg on 22.08.2016.
- */
-public class TestBase {
-}
--- src/test/java/com/dgcode/wdaft/config/Verifier.java
+++
@@ -1,7 +0,0 @@
-package com.dgcode.wdaft.config;
-
-/**
- * Created by dgjorgievski on 22.08.2016.
- */
-public class Verifier {
-}
