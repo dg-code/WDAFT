9381870ae729401ad018ac3bb6fe4cd4e7ddec26
dgjorgievski <dgjorgievski@live.com>
2016-08-26 23:10:55 +0200

    Add TestListenerAdapter It holds the methods dealing with the conditions before and after test method execution

---
+++ src/test/java/com/dgcode/wdaft/listeners/TestListenerAdapter.java
@@ -0,0 +1,18 @@
+package com.dgcode.wdaft.listeners;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+/**
+ * Created by dgjorgievski on 26.08.2016.
+ */
+public class TestListenerAdapter implements IInvokedMethodListener {
+
+    public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
+    }
+
+    public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
+    }
+
+}
